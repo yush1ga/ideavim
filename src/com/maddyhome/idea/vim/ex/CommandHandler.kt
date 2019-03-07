a60c009439f7e2a3361d604880a4e06610e52e59
Alex Plate <aleksei.plate@jetbrains.com>
2019-03-07 19:40:01 +0300

    Fixing tests

--- src/com/maddyhome/idea/vim/ex/CommandHandler.kt
+++ src/com/maddyhome/idea/vim/ex/CommandHandler.kt
@@ -24,6 +24,7 @@ import com.intellij.openapi.editor.Editor
 import com.maddyhome.idea.vim.VimPlugin
 import com.maddyhome.idea.vim.command.CommandFlags
 import com.maddyhome.idea.vim.command.CommandState
+import com.maddyhome.idea.vim.group.motion.VisualMotionGroup
 import com.maddyhome.idea.vim.handler.CaretOrder
 import com.maddyhome.idea.vim.handler.ExecuteMethodNotOverriddenException
 import com.maddyhome.idea.vim.helper.EditorHelper
@@ -152,6 +153,9 @@ abstract class CommandHandler {
     }
 
     CommandState.getInstance(editor).flags = optFlags
+    if (CommandState.getInstance(editor).mode == CommandState.Mode.VISUAL) {
+      VisualMotionGroup.exitVisual(editor)
+    }
 
     var res = true
     try {
